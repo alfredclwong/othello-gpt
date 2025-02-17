a476df4818f8d296321d38f5922e0cdceb971d9b
Christy Koh <christykoh@berkeley.edu>
2025-02-17 09:59:06 +0000

    plot_in_basis, plot_neuron_excess_kurtosis

--- src/othello_gpt/model/neurons.py
+++ src/othello_gpt/model/neurons.py
@@ -12,6 +12,8 @@ from jaxtyping import Float
 from transformer_lens import ActivationCache
 import circuitsvis as cv
 from IPython.display import HTML
+import plotly.express as px
+from plotly.subplots import make_subplots
 
 from othello_gpt.data.vis import plot_game
 from othello_gpt.model.nanoGPT import GPT, GPTConfig
@@ -43,12 +45,18 @@ device = t.device(
 size = 6
 all_squares = get_all_squares(size)
 
-probes = {
-    "otem": t.load(probe_dir / "linear_probe_20250213_221416_mid_otem.pt", weights_only=True, map_location=device),
-    "tem": t.load(probe_dir / "linear_probe_20250212_141120_mid_tem.pt", weights_only=True, map_location=device),
-    "f": t.load(probe_dir / "linear_probe_20250213_000355_mid_flips.pt", weights_only=True, map_location=device),
+# %%
+probe_names = {
+    ("ot", "oe", "om"): "linear_probe_20250213_221416_mid_otem.pt",
+    "tem": "linear_probe_20250212_141120_mid_tem.pt",
+    "f": "linear_probe_20250213_000355_mid_flips.pt",
 }
-probes = {k: p / p.norm(dim=0, keepdim=True) for k, p in probes.items()}
+probes = {}
+for names, file in probe_names.items():
+    probe = t.load(probe_dir / file, weights_only=True, map_location=device).detach()
+    probe /= probe.norm(dim=0, keepdim=True)
+    for i, n in enumerate(names):
+        probes[n] = probe[..., i, :]
 {k: p.shape for k, p in probes.items()}
 
 # %%
@@ -85,216 +93,124 @@ state_dict = convert_nanogpt_to_transformer_lens_weights(
 )
 model = HookedTransformer(hooked_cfg)
 model.load_and_process_state_dict(state_dict)
+model.eval()
 model.to(device)
 
 # %%
 def plot_in_basis(
     vectors: Float[t.Tensor, "n d_model"],
     probe: Float[t.Tensor, "d_model row col"],
-    labels: List[str] = [],
-    sort_fn: Optional[Callable] = None,
+    labels: List[str],
+    sort_by: str = "kurtosis",
     top_n: int = 0,
+    title: str = "",
 ):
     # transform data vectors, e.g. neuron w_outs to a different basis, e.g. probe and visualise
     transformed_vectors = einops.einsum(
         vectors, probe,
         "n d_model, d_model row col -> n row col",
     )
-    sorted_indices = np.argsort(sort_fn(transformed_vectors))
+    if sort_by == "kurtosis":
+        kurts = kurtosis(transformed_vectors, axis=(1, 2), fisher=False)
+        sorted_indices = np.argsort(-kurts)
+    else:
+        sorted_indices = np.arange(transformed_vectors.shape[0])
     if top_n != 0:
         sorted_indices = sorted_indices[:top_n]
     transformed_vectors = transformed_vectors[sorted_indices]
-    labels = labels[sorted_indices]
+    labels = [labels[i] for i in sorted_indices]
     plot_game(
         {"boards": transformed_vectors},
         n_cols=5,
         hovertext=transformed_vectors,
         reversed=False,
         subplot_titles=labels,
+        title=title,
     )
 
 
 # %%
-linear_probe = probes["f"][..., [2, 4]]
 n_layer = model.cfg.n_layers
 n_neuron = model.cfg.d_model * 4
-w_out = model.W_out[:, :n_neuron]
+w_out = model.W_out[:, :n_neuron].detach()
 w_out /= w_out.norm(dim=-1, keepdim=True)
-w_in = model.W_in[:, :n_neuron].transpose(1, 2)
+w_in = model.W_in[:, :n_neuron].transpose(1, 2).detach()
 w_in /= w_in.norm(dim=-1, keepdim=True)
-w_u = model.W_U[:, 1:].clone()
-w_u_board = t.zeros((w_u.shape[0], size, size), device=device)
-w_u_board.flatten(1)[:, all_squares] = w_u
+
+w_u_board = t.zeros((model.W_U.shape[0], size, size), device=device)
+w_u_board.flatten(1)[:, all_squares] = model.W_U[:, 1:]
 w_u_board /= w_u_board.norm(dim=0, keepdim=True)
+w_u_board = w_u_board.nan_to_num().detach()
+probes["u"] = einops.repeat(w_u_board, "d_model row col -> d_model row col n", n=n_layer*2+1)
 
 # %%
-# neuron_unembed_board = t.full((n_layer, n_neuron, size, size), 0.0, device=device)
-# neuron_unembed_board.flatten(-2)[..., all_squares] = einops.einsum(
-#     w_out, w_u,
-#     "n_layer n_neuron d_model, d_model n_vocab -> n_layer n_neuron n_vocab"
-# )
-# neuron_unembed_board = neuron_unembed_board.flatten(0, 1).detach().cpu()
 labels = [f"L{l} N{n}" for l in range(n_layer) for n in range(n_neuron)]
-
-sort_fn = lambda x: kurtosis(x.numpy(), axis=(1, 2), fisher=False)
-# sort by kurtosis (should find sparse neurons)
-# kurt_vals = kurtosis(neuron_unembed_board.numpy(), axis=(1, 2), fisher=False)
-# sorted_indices = np.argsort(-kurt_vals)[:128]
-
-# sort by abs value of A1 square
-# a1_abs = neuron_unembed_board[:, 0, 0].abs()
-# sorted_indices = t.argsort(-a1_abs)[:128]
-
-# neuron_unembed_board = neuron_unembed_board[sorted_indices]
-# labels = [labels[i] for i in sorted_indices]
-
-# plot_game(
-#     {"boards": neuron_unembed_board},
-#     n_cols=5,
-#     hovertext=neuron_unembed_board,
-#     reversed=False,
-#     subplot_titles=labels,
-# )
-
-plot_in_basis(w_out.flatten(0, 1).detach().cpu(), w_u_board.detach().cpu(), labels, sort_fn, top_n=50)
+plot_in_basis(w_in.flatten(0, 1), probes["u"][..., 2], labels, top_n=10, title="w_in . w_u")
+plot_in_basis(w_out.flatten(0, 1), probes["u"][..., 2], labels, top_n=10, title="w_out . w_u")
+plot_in_basis(w_in.flatten(0, 1), probes["f"][..., 2], labels, top_n=10, title="w_in . f2")
+plot_in_basis(w_out.flatten(0, 1), probes["f"][..., 2], labels, top_n=10, title="w_out . f2")
 
 # %%
-w_out_probed_board = einops.einsum(
-    w_out,
-    probes["f"][..., 0, 2],
-    "layer n_neuron d_model, d_model row col -> layer n_neuron row col"
-)
-w_out_probed_board = w_out_probed_board.flatten(0, 1).detach().cpu()
+def plot_neuron_excess_kurtosis(
+    w_in: Float[t.Tensor, "n_layer n_neuron d_model"],
+    w_out: Float[t.Tensor, "n_layer n_neuron d_model"],
+    probe: Float[t.Tensor, "d_model row col"],
+    fig: Optional[go.Figure] = None,
+    row: int = 1,
+    col: int = 1,
+):
+    w_in_probed = einops.einsum(
+        w_in, probe,
+        "n_layer n_neuron d_model, d_model row col -> n_layer n_neuron row col"
+    ).detach()
+    w_out_probed = einops.einsum(
+        w_out, probe,
+        "n_layer n_neuron d_model, d_model row col -> n_layer n_neuron row col"
+    ).detach()
+    w_in_ekurt = kurtosis(w_in_probed, axis=(2, 3), fisher=False) - 3
+    w_out_ekurt = kurtosis(w_out_probed, axis=(2, 3), fisher=False) - 3
 
-labels = [f"L{l} N{n}" for l in range(n_layer) for n in range(n_neuron)]
+    if fig is None:
+        fig = go.Figure()
 
-# sort by kurtosis (should find sparse neurons)
-kurt_vals = kurtosis(w_out_probed_board.numpy(), axis=(1, 2), fisher=False)
-# sorted_indices = np.argsort(-kurt_vals)[:50]
-sorted_indices = np.arange(50)
+    for layer in range(w_in_ekurt.shape[0]):
+        fig.add_trace(
+            go.Box(
+                y=w_in_ekurt[layer],
+                name=f'Layer {layer} w_in',
+                boxmean=True,
+            ), row=row, col=col,
+        )
+        fig.add_trace(
+            go.Box(
+                y=w_out_ekurt[layer],
+                name=f'Layer {layer} w_out',
+                boxmean=True,
+            ), row=row, col=col,
+        )
 
-w_out_probed_board = w_out_probed_board[sorted_indices]
-labels = [labels[i] for i in sorted_indices]
-
-plot_game(
-    {"boards": w_out_probed_board},
-    n_cols=5,
-    hovertext=w_out_probed_board,
-    reversed=False,
-    subplot_titles=labels,
-)
-
-# %%
-w_in_probed_board = einops.einsum(
-    w_in,
-    probes["f"][..., 0, 2],
-    "layer n_neuron d_model, d_model row col -> layer n_neuron row col"
-)
-w_in_probed_board = w_in_probed_board.flatten(0, 1).detach().cpu()
-
-labels = [f"L{l} N{n}" for l in range(n_layer) for n in range(n_neuron)]
-
-# sort by kurtosis (should find sparse neurons)
-kurt_vals = kurtosis(w_in_probed_board.numpy(), axis=(1, 2), fisher=False)
-# sorted_indices = np.argsort(-kurt_vals)[:50]
-sorted_indices = np.arange(50)
-
-w_in_probed_board = w_in_probed_board[sorted_indices]
-labels = [labels[i] for i in sorted_indices]
-
-plot_game(
-    {"boards": w_in_probed_board},
-    n_cols=5,
-    hovertext=w_in_probed_board,
-    reversed=False,
-    subplot_titles=labels,
-    title="W_in x 'their' probe"
-)
+    fig.update_layout(
+        showlegend=False,
+    )
 
 # %%
-w_in_probed_board = einops.einsum(
-    w_in,
-    probe_normed[..., 1],  # 0 = theirs, 1 = empty, 2 = mine
-    "layer n_neuron d_model, d_model row col -> layer n_neuron row col"
+layers = [2, 4]
+fig = make_subplots(
+    rows=len(layers),
+    cols=len(probes),
+    subplot_titles=[f"'{n}' L{layer}" for layer in layers for n in probes],
+    shared_yaxes=True
 )
-w_in_probed_board = w_in_probed_board.flatten(0, 1).detach().cpu()
-
-labels = [f"L{l} N{n}" for l in range(n_layer) for n in range(n_neuron)]
-
-# sort by kurtosis (should find sparse neurons)
-kurt_vals = kurtosis(w_in_probed_board.numpy(), axis=(1, 2), fisher=False)
-sorted_indices = np.argsort(-kurt_vals)[:50]
-
-w_in_probed_board = w_in_probed_board[sorted_indices]
-labels = [labels[i] for i in sorted_indices]
-
-plot_game(
-    {"boards": w_in_probed_board},
-    n_cols=5,
-    hovertext=w_in_probed_board,
-    reversed=False,
-    subplot_titles=labels,
-    title="W_in x 'empty' probe",
-)
-
-# %%
-w_in_probed_board = einops.einsum(
-    w_in,
-    probe_normed[..., 2],  # 0 = theirs, 1 = empty, 2 = mine
-    "layer n_neuron d_model, d_model row col -> layer n_neuron row col"
-)
-w_in_probed_board = w_in_probed_board.flatten(0, 1).detach().cpu()
-
-labels = [f"L{l} N{n}" for l in range(n_layer) for n in range(n_neuron)]
-
-# sort by kurtosis (should find sparse neurons)
-kurt_vals = kurtosis(w_in_probed_board.numpy(), axis=(1, 2), fisher=False)
-sorted_indices = np.argsort(-kurt_vals)[:50]
-
-w_in_probed_board = w_in_probed_board[sorted_indices]
-labels = [labels[i] for i in sorted_indices]
-
-plot_game(
-    {"boards": w_in_probed_board},
-    n_cols=5,
-    hovertext=w_in_probed_board,
-    reversed=False,
-    subplot_titles=labels,
-    title="W_in x 'mine' probe",
-)
-
-# %%
-w_in_unembed_board = t.full((n_layer, n_neuron, size, size), 0.0, device=device)
-w_in_unembed_board.flatten(-2)[..., all_squares] = einops.einsum(
-    w_in, w_u,
-    "n_layer n_neuron d_model, d_model n_vocab -> n_layer n_neuron n_vocab"
-)
-w_in_unembed_board = w_in_unembed_board.flatten(0, 1).detach().cpu()
-labels = [f"L{l} N{n}" for l in range(n_layer) for n in range(n_neuron)]
-
-# sort by kurtosis (should find sparse neurons)
-kurt_vals = kurtosis(w_in_unembed_board.numpy(), axis=(1, 2), fisher=False)
-sorted_indices = np.argsort(-kurt_vals)[:50]
-
-w_in_unembed_board = w_in_unembed_board[sorted_indices]
-labels = [labels[i] for i in sorted_indices]
-
-plot_game(
-    {"boards": w_in_unembed_board},
-    n_cols=5,
-    hovertext=w_in_unembed_board,
-    reversed=False,
-    subplot_titles=labels,
-)
+for row, layer in enumerate(layers):
+    for col, (n, p) in enumerate(probes.items()):
+        plot_neuron_excess_kurtosis(w_in, w_out, p[..., layer], fig=fig, row=row+1, col=col+1)
+fig.update_layout(height=800)
+fig.show()
 
 # %%
 # Conditional on A1 being strongly (un)predicted, which L1 neurons activated strongly?
 # Which post-L0 residual stream directions activated the L1 neurons?
 # How did the L0 block create these directions?
-cosine_similarity = t.nn.functional.cosine_similarity(
-    probe_normed[..., 0].flatten(), probe_normed[..., 2].flatten(), dim=0
-)
-print("Cosine similarity:", cosine_similarity.item())
 
 # %%
 model.W_Q.shape, model.W_K.shape, model.W_V.shape, model.W_O.shape  # n_layer n_head d_model d_head
